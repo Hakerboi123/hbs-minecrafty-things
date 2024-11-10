0b7e59be8697e40a8a33d75fe99a81e769cd8bfd
Hakerboi123 <racerburhan@gmail.com>
2024-11-10 13:38:52 +0000

    Initial commit

---
+++ src/main/java/net/hakerboi/hbsminecraftythings/HBsMinecraftyThings.java
@@ -0,0 +1,17 @@
+package net.hakerboi.hbsminecraftythings;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HBsMinecraftyThings implements ModInitializer {
+	public static final String MOD_ID = "hbs-minecrafty-things";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/hakerboi/hbsminecraftythings/HbsMinecraftyThingsClient.java
@@ -0,0 +1,10 @@
+package net.hakerboi.hbsminecraftythings;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HbsMinecraftyThingsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
